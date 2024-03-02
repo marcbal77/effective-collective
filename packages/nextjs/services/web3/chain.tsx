ce49114a261bdf8c0499a0deb8cb0a93d3babf72
Marc Balestreri <marcbalestreri@Van-Goghs-Canvas.local>
2024-03-01 18:20:15 -0700

    Initial WAGMI changes

---
+++ packages/nextjs/services/web3/chain.tsx
@@ -0,0 +1,20 @@
+import { Chain } from "wagmi";
+
+export const OasisSapphireTestnet = {
+  id: 23295,
+  name: "Oasis Sapphire Testnet",
+  network: "Oasis Sapphire Testnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "TEST",
+    symbol: "TEST",
+  },
+  rpcUrls: {
+    public: { http: ["https://testnet.sapphire.oasis.dev"] },
+    default: { http: ["https://testnet.sapphire.oasis.dev"] },
+  },
+  blockExplorers: {
+    etherscan: { name: "Sapphire", url: "https://testnet.explorer.sapphire.oasis.dev" },
+    default: { name: "Sapphire", url: "https://testnet.explorer.sapphire.oasis.dev" },
+  },
+} as const satisfies Chain;
